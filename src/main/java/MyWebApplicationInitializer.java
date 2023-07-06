1c47ef8e82185aaf34591a5e835bba182e4cad5a
AndrasFejes22 <afejes22@gmail.com>
2023-07-06 14:34:31 +0200

    Filter stuff added

--- src/main/java/MyWebApplicationInitializer.java
+++ src/main/java/MyWebApplicationInitializer.java
@@ -1,9 +1,11 @@
+import jakarta.servlet.FilterRegistration;
 import web.config.WebConfig;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import web.filter.HelloFilter;
 
 public class MyWebApplicationInitializer implements WebApplicationInitializer {
 
@@ -27,5 +29,9 @@ public class MyWebApplicationInitializer implements WebApplicationInitializer {
                 = servletContext.addFilter("webContextFilter", new RequestContextFilter());
         filterRegistration.addMappingForServletNames(null, false, "spring-web");
          */
+
+        FilterRegistration.Dynamic filterRegistration
+                = servletContext.addFilter("helloFilter", new HelloFilter());
+        filterRegistration.addMappingForServletNames(null, false, "spring-web");
     }
 }
